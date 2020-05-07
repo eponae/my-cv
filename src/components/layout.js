1b9498822e799d35c5a819e92b973772a6562aa7
Alice Rimassa <alice@eponae.fr>
2020-05-07 13:36:17 +0200

    Single quote

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react";
-import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
-import styled from "styled-components";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
+import styled from 'styled-components';
 
-import Header from "./header";
+import Header from './header';
 
 const LayoutWrapper = styled.div`
   margin: 0 auto;
